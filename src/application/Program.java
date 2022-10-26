81ffda485189a4a4297acd5ae68f9f46c2d698d8
Marcos Paulo <itsolutions031250@gmail.com>
2022-10-26 17:19:33 -0300

    Combo Devices

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,27 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
     public static void main(String[] args) {
-        Printer p = new Printer("185252544458569585225452552465465498765432154687643221");
+        ConcretePrinter p = new ConcretePrinter("185252544458569585225452552465465498765432154687643221");
         p.processDoc("My Letter");
         p.print("My Letter");
 
+        System.out.println();
 
-        Scanner s = new Scanner("129387129085731490587134901389457139804349857320958743590374509843573982405748903");
+        ConcreteScanner s = new ConcreteScanner("129387129085731490587134901389457139804349857320958743590374509843573982405748903");
         s.processDoc("My E-mail");
         System.out.println("Scan Result: " + s.scan());
+
+        System.out.println();
+
+        ComboDevice c = new ComboDevice("12334567654345654356754567543");
+        c.processDoc("My dissertation");
+        c.print("My dissertation");
+        System.out.println("Scan Result: " + c.scan());
 
     }
 }
